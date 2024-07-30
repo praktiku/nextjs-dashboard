ba299549526e1d1470acb760518bf590dad33ba1
handy <handy@gmail.com>
2024-07-30 15:38:41 +0700

    learn chapter: 5

---
+++ app/ui/fonts.ts
@@ -0,0 +1,9 @@
+import { Inter, Lustria } from "next/font/google";
+import { Lusitana } from "next/font/google";
+
+export const inter = Inter({ subsets: ["latin"] });
+
+export const lusitana = Lustria({
+  subsets: ["latin"],
+  weight: "400",
+});

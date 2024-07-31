049bd39335969885033375714d0c29f5101436cf
handy <handy@gmail.com>
2024-07-31 16:36:40 +0700

    learn chapter: 18 (completed)

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,23 @@
-export default function Page() {
-  return <p>Customers Page</p>;
+import { fetchCustomers, fetchFilteredCustomers } from "@/app/lib/data";
+import Table from "@/app/ui/customers/table";
+import { Metadata } from "next";
+export const metadata: Metadata = {
+  title: "Customers",
+};
+
+export default async function Page({
+  searchParams,
+}: {
+  searchParams?: {
+    query?: string;
+  };
+}) {
+  const query = searchParams?.query || "";
+  const customers = await fetchFilteredCustomers(query);
+
+  return (
+    <main>
+      <Table customers={customers} />
+    </main>
+  );
 }
